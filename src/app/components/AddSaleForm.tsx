dfe8e12e7d2ffdf7b6e5d64617d8b1a97315f4d3
MauricioTeruel98 <mauricioteruel98@gmail.com>
2024-09-26 10:16:34 -0300

    commit

--- src/app/components/AddSaleForm.tsx
+++ src/app/components/AddSaleForm.tsx
@@ -65,8 +65,8 @@ export default function AddSaleForm({ products, refreshData }: AddSaleFormProps)
       if (item.product_id === productId) {
         const product = products.find(p => p.id === productId)
         if (product) {
-          const newQuantity = field === 'quantity' ? Math.min(Math.max(1, value), product.stock) : value
-          const updatedItem = { ...item, [field]: newQuantity }
+          const newValue = field === 'quantity' ? Math.min(Math.max(1, value), product.stock) : value
+          const updatedItem = { ...item, [field]: newValue }
           updatedItem.subtotal = updatedItem.quantity * updatedItem.unit_price
           return updatedItem
         }
@@ -74,11 +74,11 @@ export default function AddSaleForm({ products, refreshData }: AddSaleFormProps)
       return item
     }))
   }
-  
+
   const totalSale = useMemo(() => {
     return saleItems.reduce((sum, item) => sum + Number(item.subtotal), 0)  // Convertir subtotal a número
   }, [saleItems])
-  
+
 
   console.log(totalSale);
 
@@ -188,6 +188,13 @@ export default function AddSaleForm({ products, refreshData }: AddSaleFormProps)
                           className="w-20 border-primary"
                           min="1"
                           max={product?.stock}
+                        />
+                        <Input
+                          type="number"
+                          value={item.unit_price}
+                          onChange={(e) => updateSaleItem(item.product_id, 'unit_price', parseFloat(e.target.value))}
+                          className="w-24 border-primary"
+                          min="0"
                         />
                         <span className="w-24 text-right"><b>Subtotal:</b> {formatPrice(item.subtotal)}</span>
                         <Button
